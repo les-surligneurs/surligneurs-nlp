0c840bbfd2251294656fbf5666437120dd245673
Mohamed Ramdane Debiane <mohamed.debiane@ens.uvsq.fr>
2021-09-13 18:35:09 +0200

    Mise en place env

---
+++ src/main/java/lessurligneurs/App.java
@@ -0,0 +1,13 @@
+package lessurligneurs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
